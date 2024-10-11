c659971f55e5b8ac346696a1859afa39544fae0b
Abigal Holley <abbyholley10@gmail.com>
2024-10-11 13:13:55 -0500

    New commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,55 @@
 package com.ps;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
+
+    static Scanner commandScanner = new Scanner(System.in);
+    static Scanner inputScanner = new Scanner(System.in);
+    static ArrayList<Transaction> allTransactionsInLedger = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int mainMenuCommand;
+
+        do {
+            System.out.println("Please enter an option!");
+            System.out.println("1)Look up transaction");
+            System.out.println("2)Display all transactions");
+            System.out.println("0)Exit");
+            System.out.println("Command: ");
+            try {
+                mainMenuCommand =
+                        commandScanner.nextInt();
+            } catch(InputMismatchException ime) {
+                System.out.println("Invalid. Please enter a number.");
+                commandScanner.next();
+                mainMenuCommand = 0;
+            }
+            switch (mainMenuCommand){
+                case 1:
+                    addTransaction();
+                    break;
+                case 2:
+                    displayAllTransactions();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Command not found! Please try again.");
+            }
+        } while (mainMenuCommand !=0); // Loop until user wants to stop
+
+    }
+
+    public static void displayAllTransactions(){
+      try {
+          BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"));
+          String firstLine = bufferedReader.readLine();
+      }
     }
 }
