b615745443ab134433deefd689fc0f14e4fb9052
Abigal Holley <abbyholley10@gmail.com>
2024-10-16 10:29:15 -0500

    Updated commit

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -56,138 +56,6 @@ public class Main {
 
     }
 
-
-    public static void reports(){
-        int reportMenuCommand;
-        do{
-            System.out.println("Welcome to your reports!");
-            System.out.println("What would you like to do?");
-            System.out.println("1)Display month to date");
-            System.out.println("2)Display previous month");
-            System.out.println("3)Display year to date");
-            System.out.println("4)Display previous year");
-            System.out.println("5)Search by vendor");
-            System.out.println("0)Back to home page");
-            System.out.println("Command");
-
-            try {
-                reportMenuCommand = commandScanner.nextInt();
-            }catch (InputMismatchException ime){
-                reportMenuCommand = 0;
-            }
-            switch (reportMenuCommand){
-                case 1:
-                    displayMonthToDate();
-                    break;
-                case 2:
-                    displayPreviousMonth();
-                    break;
-                case 3:
-                    displayYearToDate();
-                    break;
-                case 4:
-                    displayPreviousYear();
-                    break;
-                case 5:
-                    searchByVendor();
-                    break;
-                case 0:
-                    System.out.println("Returning to home page");
-                    break;
-                default:
-                    System.out.println("Invalid command...Try again!");
-            }
-        } while (reportMenuCommand != 0);
-
-    }
-    public static void displayMonthToDate(){
-        LocalDate currentDate = LocalDate.now();
-        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
-        for (Transaction transaction: allTransactionsInLedger){
-        if (transaction.getDate().isAfter(firstDayOfMonth)&&transaction.getDate().isBefore(currentDate))
-            System.out.println(transaction);
-        }
-    }
-    public static void displayPreviousMonth(){
-        LocalDate currentDate = LocalDate.now();
-        LocalDate previousMonthDate = currentDate.minusMonths(1);
-        for (Transaction transaction: allTransactionsInLedger){
-            if (transaction.getDate().isAfter(previousMonthDate)&&transaction.getDate().isBefore(currentDate))
-                System.out.println(transaction);
-        }
-    }
-    public static void displayYearToDate(){
-        LocalDate currentDate = LocalDate.now();
-        LocalDate firstDayOfYear = currentDate.withDayOfYear(1);
-        for (Transaction transaction: allTransactionsInLedger){
-            if (transaction.getDate().isAfter(firstDayOfYear)&&transaction.getDate().isBefore(currentDate))
-                System.out.println(transaction);
-        }
-    }
-    public static void displayPreviousYear(){
-        LocalDate currentDate = LocalDate.now();
-        LocalDate previousYearDate = currentDate.minusYears(1);
-        for (Transaction transaction: allTransactionsInLedger){
-            if (transaction.getDate().isAfter(previousYearDate)&&transaction.getDate().isBefore(currentDate))
-                System.out.println(transaction);
-        }
-    }
-    public static void searchByVendor(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter vendor name: ");
-        String searchVendor = scanner.nextLine();
-        for (Transaction transaction: allTransactionsInLedger){
-            if (transaction.getVendor().equalsIgnoreCase(searchVendor)){
-                System.out.println(transaction);
-            }
-        } scanner.close();
-    }
-
-
-
-    // Need first line of file to be skipped
-    // Need files to be read one by one
-    // Need file items to be split by a line so the data can be read easily
-
-    public static void ledger(){
-        int ledgerMenuCommand;
-        do{
-            System.out.println("Ledger screen");
-            System.out.println("1)Display all entries");
-            System.out.println("2)Display deposits going into account");
-            System.out.println("3)Display payments going out of account");
-            System.out.println("4)Display reports");
-            System.out.println("0)Home");
-            System.out.println("Command: ");
-
-            try {
-                ledgerMenuCommand = commandScanner.nextInt();
-            } catch (InputMismatchException ime){
-                ledgerMenuCommand = 0;
-            }
-            switch (ledgerMenuCommand){
-                case 1:
-                    displayAll();
-                    break;
-                case 2:
-                    displayDeposit();
-                    break;
-                case 3:
-                    displayPayments();
-                    break;
-                case 4:
-                    reports();
-                    break;
-                case 0:
-                    System.out.println("Returning to main menu");
-                    break;
-                default:
-                    System.out.println("Invalid command...");
-            }
-
-        } while (ledgerMenuCommand != 0);
-
-    }
     public static void addDeposit(){
 
         System.out.println("Please enter the details of the transaction");
@@ -242,6 +110,46 @@ public class Main {
         // come back to fix this and add a few things
         System.out.println(transaction);
     }
+    public static void ledger(){
+        int ledgerMenuCommand;
+        do{
+            System.out.println("Ledger screen");
+            System.out.println("1)Display all entries");
+            System.out.println("2)Display deposits going into account");
+            System.out.println("3)Display payments going out of account");
+            System.out.println("4)Display reports");
+            System.out.println("0)Home");
+            System.out.println("Command: ");
+
+            try {
+                ledgerMenuCommand = commandScanner.nextInt();
+            } catch (InputMismatchException ime){
+                ledgerMenuCommand = 0;
+            }
+            switch (ledgerMenuCommand){
+                case 1:
+                    displayAll();
+                    break;
+                case 2:
+                    displayDeposit();
+                    break;
+                case 3:
+                    displayPayments();
+                    break;
+                case 4:
+                    reports();
+                    break;
+                case 0:
+                    System.out.println("Returning to main menu");
+                    break;
+                default:
+                    System.out.println("Invalid command...");
+            }
+
+        } while (ledgerMenuCommand != 0);
+
+    }
+
     public static void loadAll(){
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv")); // Opens file.csv to read from it
@@ -261,7 +169,6 @@ public class Main {
                 Transaction transaction = new Transaction(date, time, description, vendor, amount);
                 allTransactionsInLedger.add(transaction);
 
-                allTransactionsInLedger.add(new Transaction(date, time, description, vendor, amount));
             }
             bufferedReader.close();
         }catch (Exception e){
@@ -269,8 +176,8 @@ public class Main {
         }
     }
     public static void displayAll(){
-      for (Transaction transaction: allTransactionsInLedger){
-         System.out.println(transaction);
+        for (Transaction transaction: allTransactionsInLedger){
+            System.out.println(transaction);
         }
     }
     public static void displayDeposit(){
@@ -303,4 +210,96 @@ public class Main {
             }
         }
     }
+
+    public static void reports(){
+        int reportMenuCommand;
+        do{
+            System.out.println("Welcome to your reports!");
+            System.out.println("What would you like to do?");
+            System.out.println("1)Display month to date");
+            System.out.println("2)Display previous month");
+            System.out.println("3)Display year to date");
+            System.out.println("4)Display previous year");
+            System.out.println("5)Search by vendor");
+            System.out.println("0)Back to home page");
+            System.out.println("Command");
+
+            try {
+                reportMenuCommand = commandScanner.nextInt();
+            }catch (InputMismatchException ime){
+                reportMenuCommand = 0;
+            }
+            switch (reportMenuCommand){
+                case 1:
+                    displayMonthToDate();
+                    break;
+                case 2:
+                    displayPreviousMonth();
+                    break;
+                case 3:
+                    displayYearToDate();
+                    break;
+                case 4:
+                    displayPreviousYear();
+                    break;
+                case 5:
+                    searchByVendor();
+                    break;
+                case 0:
+                    System.out.println("Returning to home page");
+                    break;
+                default:
+                    System.out.println("Invalid command...Try again!");
+            }
+        } while (reportMenuCommand != 0);
+
+    }
+
+    public static void displayMonthToDate(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+        for (Transaction transaction: allTransactionsInLedger){
+        if (transaction.getDate().isAfter(firstDayOfMonth)&&transaction.getDate().isBefore(currentDate))
+            System.out.println(transaction);
+        }
+    }
+    public static void displayPreviousMonth(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate previousMonthDate = currentDate.minusMonths(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(previousMonthDate)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void displayYearToDate(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfYear = currentDate.withDayOfYear(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(firstDayOfYear)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void displayPreviousYear(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate previousYearDate = currentDate.minusYears(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(previousYearDate)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void searchByVendor(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter vendor name: ");
+        String searchVendor = scanner.nextLine();
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getVendor().equalsIgnoreCase(searchVendor)){
+                System.out.println(transaction);
+            }
+        } scanner.close();
+    }
+
+    // Need first line of file to be skipped
+    // Need files to be read one by one
+    // Need file items to be split by a line so the data can be read easily
+
 }
