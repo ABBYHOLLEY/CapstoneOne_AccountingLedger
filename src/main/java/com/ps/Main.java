b18a7ef3cdaa926755b5d1d6b8f43ad00ebe9dd0
Abigal Holley <abbyholley10@gmail.com>
2024-10-15 19:49:58 -0500

    Updated commit, forgot to commit over weekend

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,14 +2,12 @@ package com.ps;
 
 import java.io.BufferedReader; // To read data
 import java.io.FileReader; // Also to read data
-import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.InputMismatchException; // Used if user enters incorrect data
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.LocalDateTime;
 
 public class Main {
     // Scanners to get data from user
@@ -18,12 +16,14 @@ public class Main {
     static ArrayList<Transaction> allTransactionsInLedger = new ArrayList<>();
 
     public static void main(String[] args) {
+        loadAll();
         int mainMenuCommand;
         // Menu for user to choose three options
         do {
-            System.out.println("Please enter an option!");
-            System.out.println("1)Look up transaction");
-            System.out.println("2)Display all transactions");
+            System.out.println("Main Menu");
+            System.out.println("1)Add Deposit");
+            System.out.println("2)Make Payment");
+            System.out.println("3)Ledger");
             System.out.println("0)Exit");
             System.out.println("Command: ");
 
@@ -32,7 +32,8 @@ public class Main {
                         commandScanner.nextInt();
             } catch(InputMismatchException ime) {// Catches any errors+ prints message
                 System.out.println("Invalid. Please enter a number.");
-                commandScanner.next();
+                ime.printStackTrace();
+                commandScanner.nextInt();
                 mainMenuCommand = 0;
             }
             switch (mainMenuCommand){
@@ -41,10 +42,11 @@ public class Main {
                     break;
                 case 2:
                     makePayment();
-                case 3:
-                    displayAllTransactions();
                     break;
-                case 4:
+                case 3:
+                    ledger();
+                    break;
+                case 0:
                     System.out.println("Exiting...");
                     break;
                 default:
@@ -54,53 +56,199 @@ public class Main {
 
     }
 
+
+    public static void reports(){
+        int reportMenuCommand;
+        do{
+            System.out.println("Welcome to your reports!");
+            System.out.println("What would you like to do?");
+            System.out.println("1)Display month to date");
+            System.out.println("2)Display previous month");
+            System.out.println("3)Display year to date");
+            System.out.println("4)Display previous year");
+            System.out.println("5)Search by vendor");
+            System.out.println("0)Back to home page");
+            System.out.println("Command");
+
+            try {
+                reportMenuCommand = commandScanner.nextInt();
+            }catch (InputMismatchException ime){
+                reportMenuCommand = 0;
+            }
+            switch (reportMenuCommand){
+                case 1:
+                    displayMonthToDate();
+                    break;
+                case 2:
+                    displayPreviousMonth();
+                    break;
+                case 3:
+                    displayYearToDate();
+                    break;
+                case 4:
+                    displayPreviousYear();
+                    break;
+                case 5:
+                    searchByVendor();
+                    break;
+                case 0:
+                    System.out.println("Returning to home page");
+                    break;
+                default:
+                    System.out.println("Invalid command...Try again!");
+            }
+        } while (reportMenuCommand != 0);
+
+    }
+    public static void displayMonthToDate(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+        for (Transaction transaction: allTransactionsInLedger){
+        if (transaction.getDate().isAfter(firstDayOfMonth)&&transaction.getDate().isBefore(currentDate))
+            System.out.println(transaction);
+        }
+    }
+    public static void displayPreviousMonth(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate previousMonthDate = currentDate.minusMonths(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(previousMonthDate)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void displayYearToDate(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfYear = currentDate.withDayOfYear(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(firstDayOfYear)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void displayPreviousYear(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate previousYearDate = currentDate.minusYears(1);
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getDate().isAfter(previousYearDate)&&transaction.getDate().isBefore(currentDate))
+                System.out.println(transaction);
+        }
+    }
+    public static void searchByVendor(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter vendor name: ");
+        String searchVendor = scanner.nextLine();
+        for (Transaction transaction: allTransactionsInLedger){
+            if (transaction.getVendor().equalsIgnoreCase(searchVendor)){
+                System.out.println(transaction);
+            }
+        } scanner.close();
+    }
+
+
+
     // Need first line of file to be skipped
     // Need files to be read one by one
     // Need file items to be split by a line so the data can be read easily
 
-    public static void displayAllTransactions(){
-      try {
-          BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv")); // Opens file.csv to read from it
+    public static void ledger(){
+        int ledgerMenuCommand;
+        do{
+            System.out.println("Ledger screen");
+            System.out.println("1)Display all entries");
+            System.out.println("2)Display deposits going into account");
+            System.out.println("3)Display payments going out of account");
+            System.out.println("4)Display reports");
+            System.out.println("0)Home");
+            System.out.println("Command: ");
 
-          String firstLine = bufferedReader.readLine();// First line skipped
-          String input;
+            try {
+                ledgerMenuCommand = commandScanner.nextInt();
+            } catch (InputMismatchException ime){
+                ledgerMenuCommand = 0;
+            }
+            switch (ledgerMenuCommand){
+                case 1:
+                    displayAll();
+                    break;
+                case 2:
+                    displayDeposit();
+                    break;
+                case 3:
+                    displayPayments();
+                    break;
+                case 4:
+                    reports();
+                    break;
+                case 0:
+                    System.out.println("Returning to main menu");
+                    break;
+                default:
+                    System.out.println("Invalid command...");
+            }
 
-          while ((input = bufferedReader.readLine()) != null){
-              String[] transactionarr = input.split("\\|");
+        } while (ledgerMenuCommand != 0);
 
-              LocalDateTime date = LocalDateTime.parse(transactionarr[0], DateTimeFormatter.ofPattern("yy-MM-dd"));
-              LocalDateTime time = LocalDateTime.parse(transactionarr[1], DateTimeFormatter.ofPattern("HH:mm:ss"));
-              String description = transactionarr[2];
-              String vendor = transactionarr[3];
-              Float amount = Float.parseFloat(transactionarr[4]);
-
-              Transaction transaction = new Transaction(date, time, description, vendor, amount);
-              allTransactionsInLedger.add(transaction);
-
-              allTransactionsInLedger.add(new Transaction(date, time, description, vendor, amount));
-          }
-          bufferedReader.close();
-      }catch (Exception e){
-          e.printStackTrace();
-        }
     }
-    public static void addTransaction(){
+    public static void addDeposit(){
+
         System.out.println("Please enter the details of the transaction");
 
-        System.out.println("Date: ");
-        LocalDateTime date = inputScanner.nextLine();
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        System.out.println("Time: ");
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 
-        System.out.println("Description: ");
+        System.out.println("Enter the transaction description: ");
         String description = inputScanner.nextLine();
 
-        System.out.println("Vendor: ");
+        System.out.println("Enter the vendor: ");
         String vendor = inputScanner.nextLine();
 
-        System.out.println("Amount: ");
-        String amount = inputScanner.nextLine();
+        System.out.println("Enter the amount: ");
+        String amountInput = inputScanner.nextLine();
+        Float amount = Float.parseFloat(amountInput);
 
+    }
+    public static void makePayment(){
+        System.out.println("Please enter the payment details");
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 
+    }
+    public static void loadAll(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv")); // Opens file.csv to read from it
+
+            String firstLine = bufferedReader.readLine();// First line skipped
+            String input;
+
+            while ((input = bufferedReader.readLine()) != null){
+                String[] transactionarr = input.split("\\|");
+
+                LocalDate date = LocalDate.parse(transactionarr[0]);
+                LocalTime time = LocalTime.parse(transactionarr[1]);
+                String description = transactionarr[2];
+                String vendor = transactionarr[3];
+                Float amount = Float.parseFloat(transactionarr[4]);
+
+                Transaction transaction = new Transaction(date, time, description, vendor, amount);
+                allTransactionsInLedger.add(transaction);
+
+                allTransactionsInLedger.add(new Transaction(date, time, description, vendor, amount));
+            }
+            bufferedReader.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void displayAll(){
+      for (Transaction transaction: allTransactionsInLedger){
+         System.out.println(transaction);
+        }
+    }
+    public static void displayDeposit(){
+        System.out.println("Testing displayDeposit");
+
+    }
+    public static void displayPayments(){
+        System.out.println("Testing displayPayments");
     }
 }
--- src/main/java/com/ps/Transaction.java
+++ src/main/java/com/ps/Transaction.java
@@ -1,8 +1,75 @@
 package com.ps;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Transaction {
-    public Transaction(LocalDateTime date, LocalDateTime time, String description, String vendor, Float amount) {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private Float amount;
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, Float amount)
+    {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date=" + date +
+                ", time=" + time +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
     }
 }
+
+
